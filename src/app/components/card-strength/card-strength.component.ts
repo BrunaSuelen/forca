e7aa70934c5af79294267618b16a2883f3580195
Bruna Suelen <subr.garcia@gmail.com>
2021-11-02 11:13:30 -0300

    feat: Adiciona estrutura estática para o componente de cardstrength

---
+++ src/app/components/card-strength/card-strength.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-strength',
+  templateUrl: './card-strength.component.html',
+  styleUrls: ['./card-strength.component.scss']
+})
+export class CardStrengthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
