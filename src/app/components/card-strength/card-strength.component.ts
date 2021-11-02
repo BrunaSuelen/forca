201a4f678a5a9da1f3555aae4fa3fa2ee0e5da99
Bruna Suelen <subr.garcia@gmail.com>
2021-11-02 16:09:41 -0300

    feat: Adiciona validação das letras informadas

--- src/app/components/card-strength/card-strength.component.ts
+++ src/app/components/card-strength/card-strength.component.ts
@@ -1,31 +1,65 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Letter } from 'src/app/model/letter.model';
 import { Word } from 'src/app/model/word.model';
+import { ALPHABET } from 'src/assets/data/alphabet';
 
 @Component({
   selector: 'app-card-strength',
   templateUrl: './card-strength.component.html',
   styleUrls: ['./card-strength.component.scss']
 })
-export class CardStrengthComponent implements OnInit, AfterViewInit {
+export class CardStrengthComponent implements OnInit, OnChanges {
 
   @Input() word?: Word;
 
+  letters!: Array<string>;
+  alphabet: Array<Letter> = ALPHABET;
+
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+
+    if (this.word) {
+      this.initAlphabet();
+      this.updateWordInput();
+    }
+  }
 
   ngOnInit(): void {
     console.log(this.word);
   }
 
-  ngAfterViewInit(): void {
-    this.updateWordInput();
+  public initAlphabet(): void {
+    if (this.word) {
+      this.letters = [...this.word.title.toLocaleUpperCase()];
+    }
+
+    this.alphabet = this.alphabet
+      .map((letter: Letter) => {
+        letter.valid = this.letters.includes(letter.char);
+        console.log(this.letters.includes(letter.char), this.letters)
+        return letter;
+      })
   }
 
   public updateWordInput(): any {
-    if (this.word) {
-      const word = [...this.word.title];
-      return word
-        .map(() => '_')
-        .join(' ');
-    }
+    const LETTERS = this.getLetters(true, true)
+      .map((letter: any) => letter.char);
+
+    return this.letters
+      .map((letter: string) => LETTERS.includes(letter) ? letter : '_')
+      .join(' ');
+  }
+
+  public getLetters(valid: boolean, select: boolean): Array<Letter> {
+    return this.alphabet
+      .filter((letter: Letter) => letter.valid == valid)
+      .filter((letter: Letter) => letter.select == select);
+  }
+
+  public validateLetter(letter: Letter) {
+    letter.select = true;
+    this.updateWordInput();
   }
 }
