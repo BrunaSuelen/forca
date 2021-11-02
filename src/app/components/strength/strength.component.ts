a6722370c4b30219f67fbfe071699e36680f2f8f
Bruna Suelen <subr.garcia@gmail.com>
2021-11-02 11:13:04 -0300

    feat: Adiciona estrutura estática para o componente de strength

---
+++ src/app/components/strength/strength.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strength',
+  templateUrl: './strength.component.html',
+  styleUrls: ['./strength.component.scss']
+})
+export class StrengthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
