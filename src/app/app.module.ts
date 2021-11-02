bcca0cc0835491ae215e650f06ffa3272ccd7b1d
Bruna Suelen <subr.garcia@gmail.com>
2021-11-02 11:20:45 -0300

    chore: Organiza componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './template/background/background.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { ChancesComponent } from './components/chances/chances.component';
+import { BackgroundComponent } from './template/background/background.component';
+import { CardStrengthComponent } from './components/card-strength/card-strength.component';
+import { StrengthComponent } from './components/strength/strength.component';
+import { InputComponent } from './components/input/input.component';
+import { TipsComponent } from './components/tips/tips.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackgroundComponent,
-    ChancesComponent
+    ChancesComponent,
+    CardStrengthComponent,
+    StrengthComponent,
+    InputComponent,
+    TipsComponent,
+    ResumeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatDividerModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
