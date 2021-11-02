07353cf822b426f9e772fc8eb12d3153334957a6
Bruna Suelen <subr.garcia@gmail.com>
2021-11-01 23:23:35 -0300

    feat: Adiciona estrutura e estilo do background sem responsividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './template/background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
