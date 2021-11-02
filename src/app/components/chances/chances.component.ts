86a385e6a1d173c91d6105ca02ba415b20a607d5
Bruna Suelen <subr.garcia@gmail.com>
2021-11-01 23:52:26 -0300

    feat: Cria novo componente para renderizar a forca

---
+++ src/app/components/chances/chances.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chances',
+  templateUrl: './chances.component.html',
+  styleUrls: ['./chances.component.scss']
+})
+export class ChancesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
